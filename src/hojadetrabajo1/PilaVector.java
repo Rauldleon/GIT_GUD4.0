9a0df34ed57f196bb497606bc721654ef7c20bb7
Didifer123 <sal15487@uvg.edu.gt>
2016-08-22 15:18:50 -0600

    PilaVector.java
    Ya comentado

--- src/hojadetrabajo1/PilaVector.java
+++ src/hojadetrabajo1/PilaVector.java
@@ -1,31 +1,62 @@
+/**
+ * PilaVector.java
+ * Clase en donde se define la pila del tipo Vector.
+ * @author Didier Salazar, 15487
+ * @author Raul de Leon, 15112
+ */
+package postfix;
 import java.util.*;
 
+/**
+ * Clase en donde se define la pila del tipo Vector.
+ * @param <T> Tipo de dato genérico.
+ */
 public class PilaVector<T> extends Pila<T>{
-	Vector vect;
+	private Vector vect;
 
-	public PilaVector(){
+    /**
+     *Metodo constructor de la clase.
+     */
+    public PilaVector(){
 		vect = new Vector();
 	}
 
-	
-	@Override
+    /**
+     * Metodo que agrega un dato a la pila del tipo vector.
+     * @param objeto dato a agregar.
+     */
+    @Override
 	public void push(T objeto) {
 		// TODO Auto-generated method stub
 		vect.add(objeto);
 	}
 
-
+    /**
+     * Metodo que obtiene de la pila del tipo vector un dato en una posicion dada.
+     * @param index posicion a obtener el dato.
+     * @return dato en la posicion ingresada.
+     */
+    @Override
 	public T pop(int index) {
 		// TODO Auto-generated method stub
 		return (T) vect.get(index);
 	}
 
-	@Override
+    /**
+     * Metodo que elimina un dato en la pila del tipo vector dada una posicion.
+     * @param index posicion a eliminar el dato de la pila.
+     */
+    @Override
 	public void eliminar(int index) {
 		// TODO Auto-generated method stub
 		vect.remove(index);
 	}
-	@Override
+
+    /**
+     * Metodo que obtiene el tamaño de la pila.
+     * @return el tamaño de la pila.
+     */
+    @Override
 	public int getSize() {
 		// TODO Auto-generated method stub
 		return vect.size();
@@ -33,3 +64,4 @@ public class PilaVector<T> extends Pila<T>{
 	
 	
 }
+
